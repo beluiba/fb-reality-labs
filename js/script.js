e76615c18eba40905aa4d3d1474c1b1a345254d9
Belen Ibanez <belenibanezm@gmail.com>
2018-09-25 22:38:54 -0300

    New Repo

---
+++ js/script.js
@@ -0,0 +1,117 @@
+(function ($) {
+  var isValid = null;
+  var form = $('.form');
+  var submitButton = form.find('button[type="submit"]')
+  var saveButton = submitButton.filter('[name="next"]');
+
+
+  var submitForm = function (e) {
+    console.log('form submit');
+    e.preventDefault();
+    if (isValid === false) {
+      isValid = null;
+      return false;
+    }
+    saveButton.trigger('click');
+  };
+
+  var submitClick = function (e) {
+    isValid = form[0].checkValidity();
+    if (false === isValid) {
+      return true;
+    }
+    e.preventDefault();
+    alert('Success');
+  };
+
+  form.submit(submitForm);
+
+  submitButton.click(submitClick);
+})(jQuery);
+
+
+
+
+
+function autocomplete(inp, arr) {
+  var currentFocus;
+
+  if(!inp) return;
+
+  inp.addEventListener("input", function (e) {
+    var a, b, i, val = this.value;
+    closeAllLists();
+    if (!val) { return false; }
+    currentFocus = -1;
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    this.parentNode.appendChild(a);
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        b = document.createElement("DIV");
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        b.addEventListener("click", function (e) {
+          inp.value = this.getElementsByTagName("input")[0].value;
+          closeAllLists();
+        });
+        a.appendChild(b);
+      }
+    }
+  });
+
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      currentFocus++;
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      currentFocus--;
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      e.preventDefault();
+      if (currentFocus > -1) {
+        if (x) x[currentFocus].click();
+      }
+    }
+  });
+
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+  });
+}
+
+var teams = ["VR Engineering", "Marketing", "Human Resources", "Sales", "Finance", "IT", "Frontend", "User Research", "Another Item 1", "Another Item 2", "Another Item 3"];
+
+autocomplete(document.getElementById("teams"), teams);
+
+$(function () {
+  $('[data-toggle="popover"]').popover();
+})
+
